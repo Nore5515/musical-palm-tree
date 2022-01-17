2d1f18144a2d59d07d213e37bec57dee6ffb33e5
Noah Reed <Nore5515@gmail.com>
2022-01-17 14:14:29 -0600

    init for timer

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Noah");
-  const [age, setAge] = useState(22);
-
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
@@ -17,24 +14,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder="e.g. JohnDoe"
-        onChangeText={(value) => setName(value)}
-      />
-      <Text>Enter age:</Text>
-      <TextInput
-        keyboardType="numeric"
-        style={styles.input}
-        placeholder="e.g. 99"
-        onChangeText={(value) => setAge(value)}
-      />
-      <Text>
-        name: {name}, age: {age}
-      </Text>
-      <Text>{seconds}</Text>
+      <Text style={{ fontSize: 50 }}>{seconds}</Text>
     </View>
   );
 }
@@ -45,12 +25,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 200,
   },
 });
